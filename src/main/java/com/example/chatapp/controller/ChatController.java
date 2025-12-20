9ce4381213b1410ff3cba89eb8a7a6a50afe836a
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2025-12-21 01:10:27 +0700

    init with Chat Websocket Server

---
+++ src/main/java/com/example/chatapp/controller/ChatController.java
@@ -0,0 +1,40 @@
+package com.example.chatapp.controller;
+
+import com.example.chatapp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+  @MessageMapping("/send-message")
+  @SendTo("/topic/messages")
+  public ChatMessage sendMessage(ChatMessage message) {
+    System.out.println("Received message: " + message.getContent());
+
+    return message;
+  }
+
+  @MessageMapping("/leave")
+  @SendTo("/topic/messages")
+  public ChatMessage leave(String username) {
+    ChatMessage newMessage = new ChatMessage();
+    newMessage.setContent(username + " has left the chat!");
+    newMessage.setSender("SYSTEM");
+    newMessage.setType("LEAVE");
+
+    return newMessage;
+  }
+
+  @MessageMapping("/join")
+  @SendTo("/topic/messages")
+  public ChatMessage join(String username) {
+    ChatMessage newMessage = new ChatMessage();
+    newMessage.setContent(username + " has joined the chat!");
+    newMessage.setSender("SYSTEM");
+    newMessage.setType("JOIN");
+
+    return newMessage;
+  }
+}
---
+++ src/main/java/com/example/chatapp/model/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.example.chatapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessage {
+  private String sender;
+  private String content;
+  private String type;
+}
