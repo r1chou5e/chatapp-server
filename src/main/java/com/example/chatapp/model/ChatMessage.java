2625b7f3439c42fbbae82500f27c0aa892f76e8f
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2026-01-04 00:07:37 +0700

    Fix: enrich the message which is sent topic (avoid bypassing)

--- src/main/java/com/example/chatapp/model/ChatMessage.java
+++ src/main/java/com/example/chatapp/model/ChatMessage.java
@@ -2,6 +2,7 @@ package com.example.chatapp.model;
 
 import java.time.Instant;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ChatMessage {
   private String roomId;
   private String sender;
--- src/main/java/com/example/chatapp/service/ChatService.java
+++ src/main/java/com/example/chatapp/service/ChatService.java
@@ -1,7 +1,10 @@
 package com.example.chatapp.service;
 
 import com.example.chatapp.model.ChatMessage;
+import com.example.chatapp.object.session.SessionInfo;
 import com.example.chatapp.store.room.RoomPresenceStore;
+import com.example.chatapp.store.user.SessionStore;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -14,6 +17,7 @@ public class ChatService {
 
   private final SimpMessageSendingOperations messagingTemplate;
   private final RoomPresenceStore roomPresenceStore;
+  private final SessionStore sessionStore;
 
   public void sendGlobalMessage(ChatMessage message) {
     log.info("[GLOBAL CHAT] {}: {}", message.getSender(), message.getContent());
@@ -24,15 +28,25 @@ public class ChatService {
     );
   }
 
-  public void sendRoomMessage(String roomId, String stompSessionId, ChatMessage message) {
+  public void sendRoomMessage(String roomId, String stompSessionId, ChatMessage rawMessage) {
     // Verify if the user is part of the room before sending the message
     if (!roomPresenceStore.isInRoom(roomId, stompSessionId)) {
       throw new IllegalStateException("Session has not join the room");
     }
 
+    // Enrich the message with sender info
+    SessionInfo info = sessionStore.getSessionInfoByStompSessionId(stompSessionId);
+
+    ChatMessage message = ChatMessage.builder()
+        .roomId(roomId)
+        .sender(info.getUsername())
+        .content(rawMessage.getContent())
+        .timestamp(Instant.now())
+        .build();
+
+    // Send the message to the room (omitted for brevity)
     log.info("[ROOM {}] {}: {}", roomId, message.getSender(), message.getContent());
 
-    // Logic to send the message to the room (omitted for brevity)
     messagingTemplate.convertAndSend(
         "/topic/rooms/" + roomId,
         message
